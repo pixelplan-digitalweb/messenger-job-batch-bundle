da355609c8722a7366b104ed6a9725314ddbc3bd
pixelplan-digitalweb <info@pixelplan.net>
2023-01-26 17:52:39 +0000

    fix interface

--- src/Messenger/Contracts/JobBatchMessageBusFactoryInterface.php
+++ src/Messenger/Contracts/JobBatchMessageBusFactoryInterface.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 namespace Pixelplan\MessengerJobBatchBundle\Messenger\Contracts;
 
-use Pixelplan\MessengerJobBatchBundle\Messenger\JobBatchMessageBus;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 interface JobBatchMessageBusFactoryInterface
 {
-    public function forMessageBus(MessageBusInterface $messageBus): JobBatchMessageBus;
+    public function forMessageBus(MessageBusInterface $messageBus): JobBatchMessageBusInterface;
 }
