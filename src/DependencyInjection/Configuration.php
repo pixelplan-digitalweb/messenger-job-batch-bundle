c12afbe5f858c5afa1612adcbe31e0829ca5cbc0
pixelplan-digitalweb <info@pixelplan.net>
2023-01-26 17:32:01 +0000

    change defaults for lock_factory and cache_pool

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -19,11 +19,11 @@ final class Configuration implements ConfigurationInterface
             ->children()
                 ->scalarNode('lock_factory')
                     ->info('The service ID of the lock factory used by this job batch processor')
-                    ->defaultValue('lock.factory')
+                    ->defaultValue('lock.factory.abstract')
                 ->end()
                 ->scalarNode('cache_pool')
                     ->info('The cache pool to use for storing the current job batch state')
-                    ->defaultValue('cache.messenger_job_batch')
+                    ->defaultValue('cache.app')
                 ->end()
             ->end()
         ;
